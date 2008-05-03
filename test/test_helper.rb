5ae42ad1fb6561e550e7b4a6ec1271fbdc29b69b
Mathieu Martin <webmat@gmail.com>
2008-05-03 17:34:30 -0400

    Adding test plumbing before going any further

---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+require File.join( File.dirname(__FILE__), '..', 'lib', 'try_require' )
+
+require 'test/unit'
+
+require 'rubygems'
+require 'redgreen'
---
+++ test/unit/try_require_test.rb
@@ -0,0 +1,7 @@
+require File.join( File.dirname(__FILE__), '..', 'test_helper' )
+
+class TryRequireTest < Test::Unit::TestCase
+  def test_truth
+    assert true
+  end
+end
