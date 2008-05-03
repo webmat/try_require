a298f7bc13240569c7824ab508bc89fefecace3f
Mathieu Martin <webmat@gmail.com>
2008-05-03 16:54:17 -0400

    Minor cosmetic modifications

--- lib/try_require.rb
+++ lib/try_require.rb
@@ -5,9 +5,8 @@ def try_require(what, &block)
     require_result = require what
     loaded = true 
 
-  rescue Exception => ex
-    puts "** Unable to require '#{what}'"
-    puts "--> #{ex.class}: #{ex.message}"
+  rescue LoadError => ex
+    puts "Unable to require '#{what}'", "#{ex.class}: #{ex.message}"
   end 
 
   yield if loaded and block_given? 
